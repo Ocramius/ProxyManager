c2ca9c405f12d8932be9e828d910007c5d74ba20
Marco Pivetta <ocramius@gmail.com>
2013-03-22 04:37:18 +0100

    Adding unit tests for the proxy factory

---
+++ tests/ProxyManagerTest/Factory/LazyLoadingValueHolderFactoryTest.php
@@ -0,0 +1,148 @@
+<?php
+/*
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * and is licensed under the MIT license.
+ */
+
+namespace ProxyManagerTest\Factory;
+
+use CG\Generator\PhpClass;
+use PHPUnit_Framework_TestCase;
+use ProxyManager\Factory\LazyLoadingValueHolderFactory;
+use ReflectionClass;
+
+/**
+ * Tests for {@see \ProxyManager\Factory\LazyLoadingValueHolderFactoryTest}
+ *
+ * @author Marco Pivetta <ocramius@gmail.com>
+ * @license MIT
+ */
+class LazyLoadingValueHolderFactoryTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $inflector;
+
+    /**
+     * @var \ProxyManager\Configuration|\PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $config;
+
+    /**
+     * {@inheritDoc}
+     */
+    public function setUp()
+    {
+        $this->config    = $this->getMock('ProxyManager\\Configuration');
+        $this->inflector = $this->getMock('ProxyManager\\Inflector\\ClassNameInflectorInterface');
+        $this
+            ->config
+            ->expects($this->any())
+            ->method('getClassNameInflector')
+            ->will($this->returnValue($this->inflector));
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @covers \ProxyManager\Factory\LazyLoadingValueHolderFactory::__construct
+     * @covers \ProxyManager\Factory\LazyLoadingValueHolderFactory::createProxy
+     */
+    public function testWillSkipAutoGeneration()
+    {
+        $className = 'foo' . uniqid();
+
+        $this->config->expects($this->any())->method('doesAutoGenerateProxies')->will($this->returnValue(false));
+
+        $this
+            ->inflector
+            ->expects($this->once())
+            ->method('getProxyClassName')
+            ->with($className)
+            ->will($this->returnValue('ProxyManagerTestAsset\\LazyLoadingMock'));
+
+        $factory     = new LazyLoadingValueHolderFactory($this->config);
+        $initializer = function () {};
+        /* @var $proxy \ProxyManagerTestAsset\LazyLoadingMock */
+        $proxy       = $factory->createProxy($className, $initializer);
+
+        $this->assertInstanceOf('ProxyManagerTestAsset\\LazyLoadingMock', $proxy);
+        $this->assertSame($initializer, $proxy->initializer);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @covers \ProxyManager\Factory\LazyLoadingValueHolderFactory::__construct
+     * @covers \ProxyManager\Factory\LazyLoadingValueHolderFactory::createProxy
+     *
+     * NOTE: serious mocking going on in here (a class is generated on-the-fly) - careful
+     */
+    public function testWillTryAutoGeneration()
+    {
+        $className      = 'foo' . uniqid();
+        $proxyClassName = 'bar' . uniqid();
+        $generator      = $this->getMock('CG\\Core\\GeneratorStrategyInterface');
+        $autoloader     = $this->getMock('ProxyManager\\Autoloader\\AutoloaderInterface');
+
+        $this->config->expects($this->any())->method('doesAutoGenerateProxies')->will($this->returnValue(true));
+        $this->config->expects($this->any())->method('getGeneratorStrategy')->will($this->returnValue($generator));
+        $this->config->expects($this->any())->method('getProxyAutoloader')->will($this->returnValue($autoloader));
+
+        $generator
+            ->expects($this->once())
+            ->method('generate')
+            ->with(
+                $this->callback(
+                    function (PhpClass $targetClass) use ($proxyClassName) {
+                        return $targetClass->getName() === $proxyClassName;
+                    }
+                )
+            );
+
+        $autoloader->expects($this->once())->method('__invoke')->with($proxyClassName);
+
+        $this
+            ->inflector
+            ->expects($this->once())
+            ->method('getProxyClassName')
+            ->with($className)
+            ->will($this->returnValue($proxyClassName));
+
+        $this
+            ->inflector
+            ->expects($this->once())
+            ->method('getUserClassName')
+            ->with($className)
+            ->will(
+                $this->returnCallback(
+                    function () use ($proxyClassName) {
+                        eval('class ' . $proxyClassName . ' extends \\ProxyManagerTestAsset\\LazyLoadingMock {}');
+
+                        return 'ProxyManagerTestAsset\\LazyLoadingMock';
+                    }
+                )
+            );
+
+        $factory     = new LazyLoadingValueHolderFactory($this->config);
+        $initializer = function () {};
+        /* @var $proxy \ProxyManagerTestAsset\LazyLoadingMock */
+        $proxy       = $factory->createProxy($className, $initializer);
+
+        $this->assertInstanceOf($proxyClassName, $proxy);
+        $this->assertSame($initializer, $proxy->initializer);
+    }
+}
