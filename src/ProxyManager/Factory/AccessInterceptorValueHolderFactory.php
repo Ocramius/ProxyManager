3cb20cbdc5cf6fbc2c9ecbd0ed795b5be51b2a31
Marco Pivetta <ocramius@gmail.com>
2013-03-27 05:12:02 +0100

    Implementing simple access interceptor value holder factory

---
+++ src/ProxyManager/Factory/AccessInterceptorValueHolderFactory.php
@@ -0,0 +1,86 @@
+<?php
+/*
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * and is licensed under the MIT license.
+ */
+
+namespace ProxyManager\Factory;
+
+use CG\Generator\PhpClass;
+use ProxyManager\Configuration;
+use ProxyManager\ProxyGenerator\AccessInterceptorValueHolderGenerator;
+use ReflectionClass;
+
+/**
+ * Factory responsible of producing proxy objects
+ *
+ * @author Marco Pivetta <ocramius@gmail.com>
+ * @license MIT
+ */
+class LazyLoadingValueHolderFactory
+{
+    /**
+     * @var \ProxyManager\Configuration
+     */
+    protected $configuration;
+
+    /**
+     * @var bool
+     */
+    protected $autoGenerate;
+
+    /**
+     * @var \ProxyManager\Inflector\ClassNameInflectorInterface
+     */
+    protected $inflector;
+
+    /**
+     * @param \ProxyManager\Configuration $configuration
+     */
+    public function __construct(Configuration $configuration)
+    {
+        $this->configuration = $configuration;
+        // localizing some properties for performance
+        $this->autoGenerate  = $this->configuration->doesAutoGenerateProxies();
+        $this->inflector     = $this->configuration->getClassNameInflector();
+    }
+
+    /**
+     * @param object     $instance           the object to be wrapped within the value holder
+     * @param \Closure[] $prefixInterceptors an array (indexed by method name) of interceptor closures to be called
+     *                                       before method logic is executed
+     * @param \Closure[] $suffixInterceptors an array (indexed by method name) of interceptor closures to be called
+     *                                       after method logic is executed
+     *
+     * @return \ProxyManager\Proxy\AccessInterceptorInterface|\ProxyManager\Proxy\ValueHolderInterface
+     */
+    public function createProxy($instance, array $prefixInterceptors = array(), array $suffixInterceptors = array())
+    {
+        $className      = get_class($instance);
+        $proxyClassName = $this->inflector->getProxyClassName($className);
+
+        if ($this->autoGenerate && ! class_exists($proxyClassName)) {
+            $className = $this->inflector->getUserClassName($className);
+            $phpClass  = new PhpClass($proxyClassName);
+            $generator = new AccessInterceptorValueHolderGenerator();
+
+            $generator->generate(new ReflectionClass($className), $phpClass);
+            $this->configuration->getGeneratorStrategy()->generate($phpClass);
+            $this->configuration->getProxyAutoloader()->__invoke($proxyClassName);
+        }
+
+        return new $proxyClassName($instance, $prefixInterceptors, $suffixInterceptors);
+    }
+}
