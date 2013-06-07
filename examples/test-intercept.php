b4228c6ebb9dfc3466335c11fb0af9921065d3d2
Marco Pivetta <ocramius@gmail.com>
2013-06-07 10:46:32 +0200

    Adding simple usage examples

---
+++ examples/test-intercept.php
@@ -0,0 +1,25 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use ProxyManager\Configuration;
+use ProxyManager\Factory\AccessInterceptorValueHolderFactory;
+
+class Foo
+{
+   public function doFoo()
+   {
+       echo "Foo!\n";
+   }
+}
+
+$config = new Configuration();
+$factory = new AccessInterceptorValueHolderFactory($config);
+
+$proxy = $factory->createProxy(
+    new Foo(),
+    array('doFoo' => function () { echo "pre-foo!\n"; }),
+    array('doFoo' => function () { echo "post-foo!\n"; })
+);
+
+$proxy->doFoo();
