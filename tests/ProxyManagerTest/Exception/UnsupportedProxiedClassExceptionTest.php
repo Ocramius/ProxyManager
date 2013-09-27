51448fe695b1d60b10f2539a5bc0d50df52f0e02
Marco Pivetta <ocramius@gmail.com>
2013-09-28 01:21:14 +0200

    Completing coverage for the graceful 5.3 failures on localized scope failures

---
+++ tests/ProxyManagerTest/Exception/UnsupportedProxiedClassExceptionTest.php
@@ -0,0 +1,45 @@
+<?php
+/*
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * and is licensed under the MIT license.
+ */
+
+namespace ProxyManagerTest\Exception;
+
+use PHPUnit_Framework_TestCase;
+use ProxyManager\Exception\UnsupportedProxiedClassException;
+
+/**
+ * Tests for {@see \ProxyManager\Exception\UnsupportedProxiedClassException}
+ *
+ * @author Marco Pivetta <ocramius@gmail.com>
+ * @license MIT
+ */
+class UnsupportedProxiedClassExceptionTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @covers \ProxyManager\Exception\UnsupportedProxiedClassException::unsupportedLocalizedReflectionProperty
+     */
+    public function testProxyDirectoryNotFound()
+    {
+        $this->assertSame(
+            'Provided reflection property "property0" of class "ProxyManagerTestAsset\ClassWithPrivateProperties" '
+            . 'is private and cannot be localized in PHP 5.3',
+            UnsupportedProxiedClassException::unsupportedLocalizedReflectionProperty(
+                new \ReflectionProperty('ProxyManagerTestAsset\ClassWithPrivateProperties', 'property0')
+            )->getMessage()
+        );
+    }
+}
