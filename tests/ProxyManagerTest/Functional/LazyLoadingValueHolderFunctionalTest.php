f372d5a67844d3b6921ec82caff71acec2a5ff7e
Marco Pivetta <ocramius@gmail.com>
2013-09-28 02:24:44 +0200

    Fixing minor PSR2 CS issues in the test suite

--- tests/ProxyManagerTest/Functional/LazyLoadingValueHolderFunctionalTest.php
+++ tests/ProxyManagerTest/Functional/LazyLoadingValueHolderFunctionalTest.php
@@ -267,7 +267,7 @@ class LazyLoadingValueHolderFunctionalTest extends PHPUnit_Framework_TestCase
         ) use (
             $initializerMatcher,
             $realInstance
-            ) {
+        ) {
             $initializer   = null;
             $wrappedObject = $realInstance;
 
