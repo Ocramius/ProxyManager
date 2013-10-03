026b4ddc63bdcbc469f16e400bc4752e29a5062b
blanchonvincent <blanchon.vincent@gmail.com>
2013-10-03 13:17:26 +0200

    Add factory & proxy generator tests

---
+++ tests/ProxyManagerTest/Factory/OverloadingFactoryTest.php
@@ -0,0 +1,148 @@
+<?php
+/*
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * and is licensed under the MIT license.
+ */
+
+namespace ProxyManagerTest\Factory;
+
+use PHPUnit_Framework_TestCase;
+use ProxyManager\Factory\OverloadingFactory;
+use ProxyManager\Generator\ClassGenerator;
+use ProxyManager\Generator\Util\UniqueIdentifierGenerator;
+use stdClass;
+
+/**
+ * Tests for {@see \ProxyManager\Factory\OverloadingFactory}
+ *
+ * @author Vincent Blanchon <blanchon.vincent@gmail.com>
+ * @license MIT
+ */
+class OverloadingFactoryTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $inflector;
+
+    /**
+     * @var \ProxyManager\Configuration|\PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $config;
+
+    /**
+     * {@inheritDoc}
+     */
+    public function setUp()
+    {
+        $this->config    = $this->getMock('ProxyManager\\Configuration');
+        $this->inflector = $this->getMock('ProxyManager\\Inflector\\ClassNameInflectorInterface');
+        $this
+            ->config
+            ->expects($this->any())
+            ->method('getClassNameInflector')
+            ->will($this->returnValue($this->inflector));
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @covers \ProxyManager\Factory\OverloadingFactory::__construct
+     * @covers \ProxyManager\Factory\OverloadingFactory::createProxy
+     */
+    public function testWillSkipAutoGeneration()
+    {
+        $instance = new stdClass();
+
+        $this
+            ->inflector
+            ->expects($this->once())
+            ->method('getProxyClassName')
+            ->with('stdClass')
+            ->will($this->returnValue('ProxyManagerTestAsset\\OverloadingObjectMock'));
+
+        $factory    = new OverloadingFactory($this->config);
+        /* @var $proxy \ProxyManagerTestAsset\OverloadingObjectMock */
+        $proxy      = $factory->createProxy($instance);
+
+        $this->assertInstanceOf('ProxyManagerTestAsset\\OverloadingObjectMock', $proxy);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @covers \ProxyManager\Factory\OverloadingFactory::__construct
+     * @covers \ProxyManager\Factory\OverloadingFactory::createProxy
+     *
+     * NOTE: serious mocking going on in here (a class is generated on-the-fly) - careful
+     */
+    public function testWillTryAutoGeneration()
+    {
+        $instance       = new stdClass();
+        $proxyClassName = UniqueIdentifierGenerator::getIdentifier('bar');
+        $generator      = $this->getMock('ProxyManager\GeneratorStrategy\\GeneratorStrategyInterface');
+        $autoloader     = $this->getMock('ProxyManager\\Autoloader\\AutoloaderInterface');
+
+        $this->config->expects($this->any())->method('getGeneratorStrategy')->will($this->returnValue($generator));
+        $this->config->expects($this->any())->method('getProxyAutoloader')->will($this->returnValue($autoloader));
+
+        $generator
+            ->expects($this->once())
+            ->method('generate')
+            ->with(
+                $this->callback(
+                    function (ClassGenerator $targetClass) use ($proxyClassName) {
+                        return $targetClass->getName() === $proxyClassName;
+                    }
+                )
+            );
+
+        // simulate autoloading
+        $autoloader
+            ->expects($this->once())
+            ->method('__invoke')
+            ->with($proxyClassName)
+            ->will(
+                $this->returnCallback(
+                    function () use ($proxyClassName) {
+                        eval(
+                            'class ' . $proxyClassName
+                            . ' extends \\ProxyManagerTestAsset\\OverloadingObjectMock {}'
+                        );
+                    }
+                )
+            );
+
+        $this
+            ->inflector
+            ->expects($this->once())
+            ->method('getProxyClassName')
+            ->with('stdClass')
+            ->will($this->returnValue($proxyClassName));
+
+        $this
+            ->inflector
+            ->expects($this->once())
+            ->method('getUserClassName')
+            ->with('stdClass')
+            ->will($this->returnValue('ProxyManagerTestAsset\\OverloadingObjectMock'));
+
+        $factory    = new OverloadingFactory($this->config);
+        /* @var $proxy \ProxyManagerTestAsset\OverloadingObjectMock */
+        $proxy      = $factory->createProxy($instance);
+
+        $this->assertInstanceOf($proxyClassName, $proxy);
+    }
+}
---
+++ tests/ProxyManagerTest/ProxyGenerator/OverloadingObjectGeneratorTest.php
@@ -0,0 +1,97 @@
+<?php
+/*
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * and is licensed under the MIT license.
+ */
+
+namespace ProxyManagerTest\ProxyGenerator;
+
+use PHPUnit_Framework_TestCase;
+use ProxyManager\Generator\ClassGenerator;
+use ProxyManager\Generator\Util\UniqueIdentifierGenerator;
+use ProxyManager\GeneratorStrategy\EvaluatingGeneratorStrategy;
+use ProxyManager\ProxyGenerator\OverloadingObjectGenerator;
+use ReflectionClass;
+
+/**
+ * Tests for {@see \ProxyManager\ProxyGenerator\OverloadingObjectGenerator}
+ *
+ * @author Vincent Blanchon <blanchon.vincent@gmail.com>
+ * @license MIT
+ *
+ * @covers \ProxyManager\ProxyGenerator\OverloadingObjectGenerator
+ */
+class OverloadingObjectGeneratorTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @dataProvider getTestedImplementations
+     *
+     * Verifies that generated code is valid and implements expected interfaces
+     */
+    public function testGeneratesValidCode($className)
+    {
+        $generator          = $this->getProxyGenerator();
+        $generatedClassName = UniqueIdentifierGenerator::getIdentifier('AbstractProxyGeneratorTest');
+        $generatedClass     = new ClassGenerator($generatedClassName);
+        $originalClass      = new ReflectionClass($className);
+        $generatorStrategy  = new EvaluatingGeneratorStrategy();
+
+        $generator->generate($originalClass, $generatedClass);
+        $generatorStrategy->generate($generatedClass);
+
+        $generatedReflection = new ReflectionClass($generatedClassName);
+
+        $this->assertEmpty(
+            array_diff($originalClass->getInterfaceNames(), $generatedReflection->getInterfaceNames())
+        );
+
+        $this->assertSame($generatedClassName, $generatedReflection->getName());
+
+        foreach ($this->getExpectedImplementedInterfaces() as $interface) {
+            $this->assertTrue($generatedReflection->implementsInterface($interface));
+        }
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    protected function getProxyGenerator()
+    {
+        return new OverloadingObjectGenerator();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function getExpectedImplementedInterfaces()
+    {
+        return array(
+            'ProxyManager\\Proxy\\OverloadingObjectInterface',
+        );
+    }
+    
+    /**
+     * @return array
+     */
+    public function getTestedImplementations()
+    {
+        return array(
+            array('ProxyManagerTestAsset\\BaseClass'),
+            array('ProxyManagerTestAsset\\ClassWithMagicMethods'),
+            array('ProxyManagerTestAsset\\ClassWithByRefMagicMethods'),
+            array('ProxyManagerTestAsset\\ClassWithMixedProperties'),
+        );
+    }
+}
