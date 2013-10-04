f59e37c2a615205ef72be80327f30c36f9f942d5
blanchonvincent <blanchon.vincent@gmail.com>
2013-10-04 12:07:37 +0200

    Add method generator tests

---
+++ tests/ProxyManagerTest/ProxyGenerator/RemoteObject/MethodGenerator/MagicGetTest.php
@@ -0,0 +1,51 @@
+<?php
+/*
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * and is licensed under the MIT license.
+ */
+
+namespace ProxyManagerTest\ProxyGenerator\RemoteObject\MethodGenerator;
+
+use ReflectionClass;
+use PHPUnit_Framework_TestCase;
+use ProxyManager\ProxyGenerator\RemoteObject\MethodGenerator\MagicGet;
+
+/**
+ * Tests for {@see \ProxyManager\ProxyGenerator\RemoteObject\MethodGenerator\MagicGet}
+ *
+ * @author Vincent Blanchon <blanchon.vincent@gmail.com>
+ * @license MIT
+ */
+class MagicGetTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @covers \ProxyManager\ProxyGenerator\RemoteObject\MethodGenerator\MagicGet::__construct
+     */
+    public function testBodyStructure()
+    {
+        $reflection   = new ReflectionClass('ProxyManagerTestAsset\\EmptyClass');
+        $adapter      = $this->getMock('Zend\\Code\\Generator\\PropertyGenerator');
+        $adapter->expects($this->any())->method('getName')->will($this->returnValue('foo'));
+
+        $magicGet     = new MagicGet($reflection, $adapter);
+
+        $this->assertSame('__get', $magicGet->getName());
+        $this->assertCount(1, $magicGet->getParameters());
+        $this->assertStringMatchesFormat(
+            'return $this->foo->call("ProxyManagerTestAsset\EmptyClass", "__get", array($name));',
+            $magicGet->getBody()
+        );
+    }
+}
