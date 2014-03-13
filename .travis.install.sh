2a9e55db9c492792c7679114d4698dd31e035b27
Markus Staab <maggus.staab@googlemail.com>
2014-03-13 12:10:35 +0100

    move travis install instructions into separate file

---
+++ .travis.install.sh
@@ -0,0 +1,17 @@
+if [ '$TRAVIS_PHP_VERSION' = 'hhvm' ]
+then 
+    sudo apt-get remove hhvm
+    sudo add-apt-repository -y ppa:mapnik/boost
+    sudo apt-get update
+    sudo apt-get install hhvm-nightly
+    php --version    
+    
+    curl -sS https://getcomposer.org/installer | php
+    php -v ResourceLimit.SocketDefaultTimeout=30 -v Http.SlowQueryThreshold=30000 composer.phar self-update
+    php -v ResourceLimit.SocketDefaultTimeout=30 -v Http.SlowQueryThreshold=30000 composer.phar update --prefer-source
+    php -v ResourceLimit.SocketDefaultTimeout=30 -v Http.SlowQueryThreshold=30000 composer.phar install --dev --prefer-source
+else
+    composer self-update
+    composer update --prefer-source
+    composer install --dev --prefer-source
+fi
