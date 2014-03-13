2a63ecd7ab686b6fd9850461a18980ad672671bf
Marco Pivetta <ocramius@gmail.com>
2014-03-13 15:45:25 +0100

    Verbose execution of the installer

--- .travis.install.sh
+++ .travis.install.sh
@@ -1,10 +1,11 @@
-if [ "$TRAVIS_PHP_VERSION" = 'hhvm' ]; then 
+set -x
+if [ "$TRAVIS_PHP_VERSION" = 'hhvm' ]; then
     sudo apt-get remove hhvm
     sudo add-apt-repository -y ppa:mapnik/boost
     sudo apt-get update
     sudo apt-get install hhvm-nightly
-    php --version    
-    
+    php --version
+
     curl -sS https://getcomposer.org/installer | php
     php -v ResourceLimit.SocketDefaultTimeout=30 -v Http.SlowQueryThreshold=30000 composer.phar self-update
     php -v ResourceLimit.SocketDefaultTimeout=30 -v Http.SlowQueryThreshold=30000 composer.phar update --prefer-source
