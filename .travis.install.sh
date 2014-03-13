957de8648d64d2d0a3777fec793b52c198d39c82
Marco Pivetta <ocramius@gmail.com>
2014-03-13 16:11:12 +0100

    Explicitly using HHVM for test setup

--- .travis.install.sh
+++ .travis.install.sh
@@ -4,12 +4,12 @@ if [ "$TRAVIS_PHP_VERSION" = 'hhvm' ]; then
     sudo add-apt-repository -y ppa:mapnik/boost
     sudo apt-get update
     sudo apt-get install hhvm-nightly
-    php --version
+    hhvm --version
 
     curl -sS https://getcomposer.org/installer | php
-    php -v ResourceLimit.SocketDefaultTimeout=30 -v Http.SlowQueryThreshold=30000 composer.phar self-update
-    php -v ResourceLimit.SocketDefaultTimeout=30 -v Http.SlowQueryThreshold=30000 composer.phar update --prefer-source
-    php -v ResourceLimit.SocketDefaultTimeout=30 -v Http.SlowQueryThreshold=30000 composer.phar install --dev --prefer-source
+    hhvm -v ResourceLimit.SocketDefaultTimeout=30 -v Http.SlowQueryThreshold=30000 composer.phar self-update
+    hhvm -v ResourceLimit.SocketDefaultTimeout=30 -v Http.SlowQueryThreshold=30000 composer.phar update --prefer-source
+    hhvm -v ResourceLimit.SocketDefaultTimeout=30 -v Http.SlowQueryThreshold=30000 composer.phar install --dev --prefer-source
 else
     composer self-update
     composer update --prefer-source
