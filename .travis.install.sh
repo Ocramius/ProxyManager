fdf83afa905a6069b14aa63313bc96a3d8ab468a
Marco Pivetta <ocramius@gmail.com>
2014-03-21 20:47:03 +0100

    Option `-q` does not exist for `add-apt-repository`

--- .travis.install.sh
+++ .travis.install.sh
@@ -2,7 +2,7 @@ set -x
 if [ "$TRAVIS_PHP_VERSION" = 'hhvm' ]; then
     export DEBIAN_FRONTEND=noninteractive
     sudo apt-get remove -q -y hhvm
-    sudo add-apt-repository -q -y ppa:mapnik/boost
+    sudo add-apt-repository -y ppa:mapnik/boost
     sudo apt-get update -q -y
     sudo apt-get install -q -y hhvm-nightly
     hhvm --version
