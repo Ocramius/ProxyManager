17e435c2c9391299969a4a4a2c956d8d87b5b658
Marco Pivetta <ocramius@gmail.com>
2014-05-24 21:54:30 +0200

    Manual HHVM upgrade is not necessary

--- .travis.install.sh
+++ .travis.install.sh
@@ -1,15 +1,7 @@
 set -x
-if [ "$TRAVIS_PHP_VERSION" = 'hhvm' ]; then
-    export DEBIAN_FRONTEND=noninteractive
-    sudo apt-get purge -q -y hhvm
-    sudo add-apt-repository -y ppa:mapnik/boost
-    sudo apt-get update -q -y
-    sudo apt-get install -q -y hhvm-nightly
-    hhvm --version
-
+if [ "$TRAVIS_PHP_VERSION" = 'hhvm' ] || [ "$TRAVIS_PHP_VERSION" = 'hhvm-nightly' ] ; then
     curl -sS https://getcomposer.org/installer > composer-installer.php
     hhvm composer-installer.php
-    hhvm -v ResourceLimit.SocketDefaultTimeout=30 -v Http.SlowQueryThreshold=30000 composer.phar self-update
     hhvm -v ResourceLimit.SocketDefaultTimeout=30 -v Http.SlowQueryThreshold=30000 composer.phar update --prefer-source
     hhvm -v ResourceLimit.SocketDefaultTimeout=30 -v Http.SlowQueryThreshold=30000 composer.phar install --dev --prefer-source
 else
