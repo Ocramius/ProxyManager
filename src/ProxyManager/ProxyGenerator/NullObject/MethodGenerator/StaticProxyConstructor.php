ee763d3016d9f590b9a8cf95893fe6b25f51a349
Marco Pivetta <ocramius@gmail.com>
2014-09-27 21:04:28 +0200

    Implementing `staticProxyConstructor` for null objects

---
+++ src/ProxyManager/ProxyGenerator/NullObject/MethodGenerator/StaticProxyConstructor.php
@@ -0,0 +1,57 @@
+<?php
+/*
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * and is licensed under the MIT license.
+ */
+
+namespace ProxyManager\ProxyGenerator\NullObject\MethodGenerator;
+
+use ReflectionClass;
+use ReflectionProperty;
+use ProxyManager\Generator\MethodGenerator;
+
+/**
+ * The `staticProxyConstructor` implementation for null object proxies
+ *
+ * @author Marco Pivetta <ocramius@gmail.com>
+ * @license MIT
+ */
+class StaticProxyConstructor extends MethodGenerator
+{
+    /**
+     * Constructor
+     *
+     * @param ReflectionClass $originalClass Reflection of the class to proxy
+     */
+    public function __construct(ReflectionClass $originalClass)
+    {
+        parent::__construct('staticProxyConstructor', array(), static::FLAG_PUBLIC | static::FLAG_STATIC);
+
+        /* @var $publicProperties \ReflectionProperty[] */
+        $publicProperties = $originalClass->getProperties(ReflectionProperty::IS_PUBLIC);
+        $nullableProperties  = array();
+
+        foreach ($publicProperties as $publicProperty) {
+            $nullableProperties[] = '$body->' . $publicProperty->getName() . ' = null;';
+        }
+
+        $this->setDocblock("Constructor for null object initialization");
+        $this->setBody(
+            '$instance = (new \ReflectionClass(get_class()))->newInstanceWithoutConstructor();' . "\n\n"
+            . ($nullableProperties ? implode("\n", $nullableProperties) . "\n\n" : '')
+            . 'return $instance;'
+        );
+    }
+}
