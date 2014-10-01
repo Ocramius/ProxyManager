c3499f24079a9008ccca5d4d328b897773e6c8f2
Marco Pivetta <ocramius@gmail.com>
2014-10-01 18:39:23 +0200

    Coverage for the static proxy constructor implementation

---
+++ tests/ProxyManagerTest/ProxyGenerator/NullObject/MethodGenerator/StaticProxyConstructorTest.php
@@ -0,0 +1,79 @@
+<?php
+/*
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * and is licensed under the MIT license.
+ */
+
+namespace ProxyManagerTest\ProxyGenerator\NullObject\MethodGenerator;
+
+use PHPUnit_Framework_TestCase;
+use ProxyManager\ProxyGenerator\NullObject\MethodGenerator\Constructor;
+use ProxyManager\ProxyGenerator\NullObject\MethodGenerator\StaticProxyConstructor;
+use ReflectionClass;
+
+/**
+ * Tests for {@see \ProxyManager\ProxyGenerator\NullObject\MethodGenerator\StaticProxyConstructor}
+ *
+ * @author Marco Pivetta <ocramius@gmail.com>
+ * @license MIT
+ *
+ * @covers \ProxyManager\ProxyGenerator\NullObject\MethodGenerator\StaticProxyConstructor
+ * @group Coverage
+ */
+class StaticProxyConstructorTest extends PHPUnit_Framework_TestCase
+{
+    public function testBodyStructure()
+    {
+        $constructor = new StaticProxyConstructor(
+            new ReflectionClass('ProxyManagerTestAsset\\ClassWithMixedProperties')
+        );
+
+        $this->assertSame('staticProxyConstructor', $constructor->getName());
+        $this->assertCount(0, $constructor->getParameters());
+        $this->assertSame(
+            'static $reflection;
+
+$reflection = $reflection ?: $reflection = new \ReflectionClass(__CLASS__);
+$instance = (new \ReflectionClass(get_class()))->newInstanceWithoutConstructor();
+
+$instance->publicProperty0 = null;
+$instance->publicProperty1 = null;
+$instance->publicProperty2 = null;
+
+return $instance;',
+            $constructor->getBody()
+        );
+    }
+
+    public function testBodyStructureWithoutPublicProperties()
+    {
+        $constructor = new StaticProxyConstructor(
+            new ReflectionClass('ProxyManagerTestAsset\\ClassWithPrivateProperties')
+        );
+
+        $this->assertSame('staticProxyConstructor', $constructor->getName());
+        $this->assertCount(0, $constructor->getParameters());
+        $body = $constructor->getBody();
+        $this->assertSame(
+            'static $reflection;
+
+$reflection = $reflection ?: $reflection = new \ReflectionClass(__CLASS__);
+$instance = (new \ReflectionClass(get_class()))->newInstanceWithoutConstructor();
+
+return $instance;',
+            $body
+        );
+    }
+}
