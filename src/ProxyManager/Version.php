ff246e7569744e4a15dc2b043777230d82813d2f
Marco Pivetta <ocramius@gmail.com>
2014-11-08 09:47:35 +0100

    Bumping version to 1.0.0-dev

--- src/ProxyManager/Version.php
+++ src/ProxyManager/Version.php
@@ -28,7 +28,7 @@ namespace ProxyManager;
  */
 final class Version
 {
-    const VERSION = '1.0.0-beta3';
+    const VERSION = '1.0.0-dev';
 
     /**
      * Private constructor - this class is not meant to be instantiated
