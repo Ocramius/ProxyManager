dce2ed6c3372c0b46f11cb76f11d6eac87164870
Marco Pivetta <ocramius@gmail.com>
2014-12-14 07:20:42 +0100

    Syntax error fix

--- .travis.coverage.sh
+++ .travis.coverage.sh
@@ -1,5 +1,6 @@
 set -x
 if [ "$TRAVIS_PHP_VERSION" = 'hhvm' ] || [ "$TRAVIS_PHP_VERSION" = 'hhvm-nightly' ] ; then
+    echo "skipping coverage submission for HHVM builds"
 else
     wget https://scrutinizer-ci.com/ocular.phar
     php ocular.phar code-coverage:upload --format=php-clover ./clover.xml
