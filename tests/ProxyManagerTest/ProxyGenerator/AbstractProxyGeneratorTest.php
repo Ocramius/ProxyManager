2af7ad0c952ccd8dc9a9d1aeb407692e00a255d8
Marco Pivetta <ocramius@gmail.com>
2015-01-02 17:59:27 +0100

    Adding missing docblock parameters

--- tests/ProxyManagerTest/ProxyGenerator/AbstractProxyGeneratorTest.php
+++ tests/ProxyManagerTest/ProxyGenerator/AbstractProxyGeneratorTest.php
@@ -43,6 +43,8 @@ abstract class AbstractProxyGeneratorTest extends PHPUnit_Framework_TestCase
      * @dataProvider getTestedImplementations
      *
      * Verifies that generated code is valid and implements expected interfaces
+     *
+     * @param string $className
      */
     public function testGeneratesValidCode($className)
     {
