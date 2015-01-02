f7c614af9c5d755a5f54073bcf645fce6b19460e
Marco Pivetta <ocramius@gmail.com>
2015-01-02 17:47:22 +0100

    Adding missing docblock parameters

--- tests/ProxyManagerTest/Functional/NullObjectFunctionalTest.php
+++ tests/ProxyManagerTest/Functional/NullObjectFunctionalTest.php
@@ -41,6 +41,11 @@ class NullObjectFunctionalTest extends PHPUnit_Framework_TestCase
 {
     /**
      * @dataProvider getProxyMethods
+     *
+     * @param string  $className
+     * @param object  $instance
+     * @param string  $method
+     * @param mixed[] $params
      */
     public function testMethodCalls($className, $instance, $method, $params)
     {
