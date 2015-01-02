453995aef55f866d3fd11fa1a7bfbc348aefae9e
Marco Pivetta <ocramius@gmail.com>
2015-01-02 17:33:44 +0100

    Adding missing docblock parameters

--- tests/ProxyManagerTest/Functional/AccessInterceptorScopeLocalizerFunctionalTest.php
+++ tests/ProxyManagerTest/Functional/AccessInterceptorScopeLocalizerFunctionalTest.php
@@ -214,6 +214,10 @@ class AccessInterceptorScopeLocalizerFunctionalTest extends PHPUnit_Framework_Te
 
     /**
      * @dataProvider getPropertyAccessProxies
+     *
+     * @param object                                         $instance
+     * @param \ProxyManager\Proxy\AccessInterceptorInterface $proxy
+     * @param string                                         $publicProperty
      */
     public function testPropertyExistence($instance, $proxy, $publicProperty)
     {
