f7bc255301f3e35f08eaf450c0b288dbb605cd11
Marco Pivetta <ocramius@gmail.com>
2015-01-02 17:41:04 +0100

    Adding missing docblock parameters

--- tests/ProxyManagerTest/Functional/AccessInterceptorValueHolderFunctionalTest.php
+++ tests/ProxyManagerTest/Functional/AccessInterceptorValueHolderFunctionalTest.php
@@ -158,6 +158,12 @@ class AccessInterceptorValueHolderFunctionalTest extends PHPUnit_Framework_TestC
 
     /**
      * @dataProvider getProxyMethods
+     *
+     * @param string  $className
+     * @param object  $instance
+     * @param string  $method
+     * @param mixed[] $params
+     * @param mixed   $expectedValue
      */
     public function testMethodCallsAfterCloning($className, $instance, $method, $params, $expectedValue)
     {
