e942958f37427b4a707b27ebc8154e47c3b1fa27
Marco Pivetta <ocramius@gmail.com>
2015-01-02 15:41:11 +0100

    Test code should only verify instance properties

--- tests/ProxyManagerTest/Functional/LazyLoadingGhostFunctionalTest.php
+++ tests/ProxyManagerTest/Functional/LazyLoadingGhostFunctionalTest.php
@@ -27,6 +27,7 @@ use ProxyManager\GeneratorStrategy\EvaluatingGeneratorStrategy;
 use ProxyManager\GeneratorStrategy\FileWriterGeneratorStrategy;
 use ProxyManager\Proxy\GhostObjectInterface;
 use ProxyManager\ProxyGenerator\LazyLoadingGhostGenerator;
+use ProxyManager\ProxyGenerator\Util\Properties;
 use ProxyManagerTestAsset\BaseClass;
 use ProxyManagerTestAsset\ClassWithCollidingPrivateInheritedProperties;
 use ProxyManagerTestAsset\ClassWithCounterConstructor;
@@ -573,7 +574,9 @@ class LazyLoadingGhostFunctionalTest extends PHPUnit_Framework_TestCase
             }
         );
 
-        foreach ((new ReflectionClass(ClassWithMixedProperties::class))->getProperties() as $property) {
+        $reflectionClass = new ReflectionClass(ClassWithMixedProperties::class);
+
+        foreach (Properties::fromReflectionClass($reflectionClass)->getInstanceProperties() as $property) {
             $property->setAccessible(true);
 
             $this->assertSame(str_replace('Property', '', $property->getName()), $property->getValue($proxy));
