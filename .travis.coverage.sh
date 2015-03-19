8ba66cd85652e0e7ff91a68b21661f3f8ab0ecfc
Marco Pivetta <ocramius@gmail.com>
2015-03-19 23:23:57 +0000

    Submitting coverage only for PHP 5.6 builds

--- .travis.coverage.sh
+++ .travis.coverage.sh
@@ -1,7 +1,5 @@
 set -x
-if [ "$TRAVIS_PHP_VERSION" = 'hhvm' ] || [ "$TRAVIS_PHP_VERSION" = 'hhvm-nightly' ] ; then
-    echo "skipping coverage submission for HHVM builds"
-else
+if [ "$TRAVIS_PHP_VERSION" = '5.6' ] ; then
     wget https://scrutinizer-ci.com/ocular.phar
     php ocular.phar code-coverage:upload --format=php-clover ./clover.xml
 fi
