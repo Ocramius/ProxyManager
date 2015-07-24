c16b13ff9600b5ca86ab62fe68b43ef9fc3e3652
Marco Pivetta <ocramius@gmail.com>
2015-07-24 11:29:45 +0100

    Correcting init script to use system TMP dir

--- tests/language-feature-scripts/init.php
+++ tests/language-feature-scripts/init.php
@@ -7,13 +7,9 @@ require_once __DIR__ . '/../../vendor/autoload.php';
 
 $configuration = new Configuration();
 
-$locator = new \ProxyManager\FileLocator\FileLocator(__DIR__ . '/../../tmp');
+$locator = new \ProxyManager\FileLocator\FileLocator(sys_get_temp_dir());
 
 $configuration->setProxyAutoloader(
     new \ProxyManager\Autoloader\Autoloader($locator, $configuration->getClassNameInflector())
 );
-$configuration->setGeneratorStrategy(
-    new \ProxyManager\GeneratorStrategy\FileWriterGeneratorStrategy(
-        new \ProxyManager\FileLocator\FileLocator(__DIR__ . '/../../tmp')
-    )
-);
+$configuration->setGeneratorStrategy(new \ProxyManager\GeneratorStrategy\FileWriterGeneratorStrategy($locator));
