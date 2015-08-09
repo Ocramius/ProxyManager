b027c95a4f417815c39615338d51f917feb4d0f4
Marco Pivetta <ocramius@gmail.com>
2015-08-09 23:38:38 +0100

    Reverting to `EvaluatingGeneratorStrategy`

--- tests/language-feature-scripts/init.php
+++ tests/language-feature-scripts/init.php
@@ -7,9 +7,4 @@ require_once __DIR__ . '/../../vendor/autoload.php';
 
 $configuration = new Configuration();
 
-$locator = new \ProxyManager\FileLocator\FileLocator(sys_get_temp_dir());
-
-$configuration->setProxyAutoloader(
-    new \ProxyManager\Autoloader\Autoloader($locator, $configuration->getClassNameInflector())
-);
-$configuration->setGeneratorStrategy(new \ProxyManager\GeneratorStrategy\FileWriterGeneratorStrategy($locator));
+$configuration->setGeneratorStrategy(new EvaluatingGeneratorStrategy());
