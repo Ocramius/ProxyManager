909cf1398dc8948838a77924996c4123e661020e
Marco Pivetta <ocramius@gmail.com>
2015-12-30 16:35:48 +0100

    #265 - Simplified logic in `InterceptedMethod` (no need for `instanceof` checks)

--- src/ProxyManager/ProxyGenerator/AccessInterceptorValueHolder/MethodGenerator/InterceptedMethod.php
+++ src/ProxyManager/ProxyGenerator/AccessInterceptorValueHolder/MethodGenerator/InterceptedMethod.php
@@ -19,7 +19,6 @@
 namespace ProxyManager\ProxyGenerator\AccessInterceptorValueHolder\MethodGenerator;
 
 use ProxyManager\Generator\MethodGenerator;
-use ProxyManager\Generator\ParameterGenerator;
 use ProxyManager\ProxyGenerator\AccessInterceptorValueHolder\MethodGenerator\Util\InterceptorGenerator;
 use Zend\Code\Generator\PropertyGenerator;
 use Zend\Code\Reflection\MethodReflection;
@@ -51,11 +50,7 @@ class InterceptedMethod extends MethodGenerator
         $forwardedParams = [];
 
         foreach ($originalMethod->getParameters() as $parameter) {
-            $variadicPrefix = ($parameter instanceof ParameterGenerator && $parameter->isVariadic())
-                ? '...' :
-                '';
-
-            $forwardedParams[]   = $variadicPrefix . '$' . $parameter->getName();
+            $forwardedParams[] = ($parameter->isVariadic() ? '...' : '') . '$' . $parameter->getName();
         }
 
         $method->setDocblock('{@inheritDoc}');
