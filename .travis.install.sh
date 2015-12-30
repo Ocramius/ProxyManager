d1105ac0d48459b3c18dff1a1a74060f2f07f635
Marco Pivetta <ocramius@gmail.com>
2015-12-30 19:11:12 +0100

    HHVM-nightly is no more

--- .travis.install.sh
+++ .travis.install.sh
@@ -1,5 +1,5 @@
 set -x
-if [ "$TRAVIS_PHP_VERSION" = 'hhvm' ] || [ "$TRAVIS_PHP_VERSION" = 'hhvm-nightly' ] ; then
+if [ "$TRAVIS_PHP_VERSION" = 'hhvm' ] ; then
     curl -sS https://getcomposer.org/installer > composer-installer.php
     hhvm composer-installer.php
     hhvm -v ResourceLimit.SocketDefaultTimeout=30 -v Http.SlowQueryThreshold=30000 composer.phar update --prefer-source
