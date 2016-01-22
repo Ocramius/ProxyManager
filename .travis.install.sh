d3394fc9e7cd595da44a2a54d4a416d0657933c3
Marco Pivetta <ocramius@gmail.com>
2016-01-22 18:19:57 +0100

    #274 - `composer update` is sufficient

--- .travis.install.sh
+++ .travis.install.sh
@@ -3,9 +3,7 @@ if [ "$TRAVIS_PHP_VERSION" = 'hhvm' ] ; then
     curl -sS https://getcomposer.org/installer > composer-installer.php
     hhvm composer-installer.php
     hhvm -v ResourceLimit.SocketDefaultTimeout=30 -v Http.SlowQueryThreshold=30000 composer.phar update --prefer-source
-    hhvm -v ResourceLimit.SocketDefaultTimeout=30 -v Http.SlowQueryThreshold=30000 composer.phar install --dev --prefer-source
 else
     composer self-update
     composer update --prefer-source
-    composer install --dev --prefer-source
 fi
