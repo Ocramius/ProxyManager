ab9a75392e3ef83858771237494866e71850c6b2
Marco Pivetta <ocramius@gmail.com>
2016-01-22 03:39:55 +0100

    #276 - basic specification for an "unset properties" snippet generator

---
+++ tests/ProxyManagerTest/ProxyGenerator/Util/UnsetPropertiesGeneratorTest.php
@@ -0,0 +1,70 @@
+<?php
+/*
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * and is licensed under the MIT license.
+ */
+
+namespace ProxyManagerTest\ProxyGenerator\Util;
+
+use PHPUnit_Framework_TestCase;
+use ProxyManager\ProxyGenerator\Util\Properties;
+use ProxyManager\ProxyGenerator\Util\PublicScopeSimulator;
+use ProxyManager\ProxyGenerator\Util\UnsetPropertiesGenerator;
+use ProxyManagerTestAsset\EmptyClass;
+use Zend\Code\Generator\PropertyGenerator;
+
+/**
+ * Tests for {@see \ProxyManager\ProxyGenerator\Util\PublicScopeSimulator}
+ *
+ * @author Marco Pivetta <ocramius@gmail.com>
+ * @license MIT
+ *
+ * @covers \ProxyManager\ProxyGenerator\Util\PublicScopeSimulator
+ * @group Coverage
+ */
+class UnsetPropertiesGeneratorTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @dataProvider classNamesProvider
+     *
+     * @param string $className
+     * @param string $expectedCode
+     * @param string $instanceName
+     */
+    public function testGeneratedCode(string $className, string $expectedCode, string $instanceName)
+    {
+        self::assertSame(
+            $expectedCode,
+            UnsetPropertiesGenerator::generateSnippet(
+                Properties::fromReflectionClass(new \ReflectionClass($className)),
+                $instanceName
+            )
+        );
+    }
+
+    /**
+     * @return string[][]
+     */
+    public function classNamesProvider() : array
+    {
+        return [
+            EmptyClass::class => [
+                EmptyClass::class,
+                '',
+                'foo',
+            ],
+        ];
+    }
+}
