091b6d1c9bba0c4490b39c04a5b2e774c49c5c36
Marco Pivetta <ocramius@gmail.com>
2016-09-16 15:58:16 +0100

    `iterable` return type should be generated in implementing classes

--- tests/ProxyManagerTestAsset/ReturnTypeHintedInterface.php
+++ tests/ProxyManagerTestAsset/ReturnTypeHintedInterface.php
@@ -36,6 +36,8 @@ interface ReturnTypeHintedInterface
 
     public function returnSelf() : self;
 
+    public function returnIterable() : iterable;
+
     public function returnVoid() : void;
 
     public function returnSameClass() : ReturnTypeHintedInterface;
