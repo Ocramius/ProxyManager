e0072ade40c13af31cd73c5236ebc2b5a0a72554
Marco Pivetta <ocramius@gmail.com>
2016-10-22 08:50:22 -0700

    As per #344, proxied magic `__set` should also defer execution to a pre-existing method, where applicable

--- tests/ProxyManagerTest/ProxyGenerator/LazyLoadingValueHolder/MethodGenerator/MagicSetTest.php
+++ tests/ProxyManagerTest/ProxyGenerator/LazyLoadingValueHolder/MethodGenerator/MagicSetTest.php
@@ -23,6 +23,7 @@ namespace ProxyManagerTest\ProxyGenerator\LazyLoadingValueHolder\MethodGenerator
 use PHPUnit_Framework_TestCase;
 use ProxyManager\ProxyGenerator\LazyLoadingValueHolder\MethodGenerator\MagicSet;
 use ProxyManager\ProxyGenerator\PropertyGenerator\PublicPropertiesMap;
+use ProxyManagerTestAsset\ClassWithMagicMethods;
 use ProxyManagerTestAsset\EmptyClass;
 use ReflectionClass;
 use Zend\Code\Generator\PropertyGenerator;
@@ -34,12 +35,10 @@ use Zend\Code\Generator\PropertyGenerator;
  * @license MIT
  *
  * @group Coverage
+ * @covers \ProxyManager\ProxyGenerator\LazyLoadingValueHolder\MethodGenerator\MagicSet
  */
 class MagicSetTest extends PHPUnit_Framework_TestCase
 {
-    /**
-     * @covers \ProxyManager\ProxyGenerator\LazyLoadingValueHolder\MethodGenerator\MagicSet::__construct
-     */
     public function testBodyStructure()
     {
         $reflection       = new ReflectionClass(EmptyClass::class);
@@ -68,6 +67,43 @@ class MagicSetTest extends PHPUnit_Framework_TestCase
             . "if (isset(self::\$bar[\$name])) {\n    return (\$this->bar->\$name = \$value);\n}"
             . '%areturn %s;',
             $magicSet->getBody()
+        );
+    }
+
+    /**
+     * @group 344
+     *
+     * @return void
+     */
+    public function testBodyStructureWithPreExistingMagicMethod()
+    {
+        $reflection       = new ReflectionClass(ClassWithMagicMethods::class);
+        /* @var $initializer PropertyGenerator|\PHPUnit_Framework_MockObject_MockObject */
+        $initializer      = $this->createMock(PropertyGenerator::class);
+        /* @var $valueHolder PropertyGenerator|\PHPUnit_Framework_MockObject_MockObject */
+        $valueHolder      = $this->createMock(PropertyGenerator::class);
+        /* @var $publicProperties PublicPropertiesMap|\PHPUnit_Framework_MockObject_MockObject */
+        $publicProperties = $this
+            ->getMockBuilder(PublicPropertiesMap::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $initializer->expects(self::any())->method('getName')->will(self::returnValue('foo'));
+        $valueHolder->expects(self::any())->method('getName')->will(self::returnValue('bar'));
+        $publicProperties->expects(self::any())->method('isEmpty')->will(self::returnValue(false));
+        $publicProperties->expects(self::any())->method('getName')->will(self::returnValue('bar'));
+
+        $magicSet = new MagicSet($reflection, $initializer, $valueHolder, $publicProperties);
+
+        self::assertSame('__set', $magicSet->getName());
+        self::assertCount(2, $magicSet->getParameters());
+        self::assertStringMatchesFormat(
+            "\$this->foo && \$this->foo->__invoke(\$this->bar, \$this, "
+            . "'__set', array('name' => \$name, 'value' => \$value), \$this->foo);\n\n"
+            . "if (isset(self::\$bar[\$name])) {\n    return (\$this->bar->\$name = \$value);\n}\n\n"
+            . 'return $this->bar->__set($name, $value);',
+            $magicSet->getBody(),
+            'Execution is deferred to pre-existing `__set` if the property is not accessible'
         );
     }
 }
