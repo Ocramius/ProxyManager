25c3693b5f61fa1822cd4fbbb913b64e2ae6d640
Edvin Hultberg <hultberg@mittnett.net>
2018-08-17 13:39:19 +0200

    FileWriterGeneratorStrategy: Set file permissions when writing proxy file
    Ported for 2.1.x

--- tests/ProxyManagerTest/GeneratorStrategy/FileWriterGeneratorStrategyTest.php
+++ tests/ProxyManagerTest/GeneratorStrategy/FileWriterGeneratorStrategyTest.php
@@ -61,6 +61,13 @@ class FileWriterGeneratorStrategyTest extends PHPUnit_Framework_TestCase
         self::assertGreaterThan(0, strpos($body, $className));
         self::assertFalse(class_exists($fqcn, false));
         self::assertFileExists($tmpFile);
+        self::assertFileIsReadable($tmpFile);
+
+        // a user not on php.net recommended calling this as we have just called chmod on a file.
+        clearstatcache();
+
+        $perm = 0666 & ~umask();
+        self::assertTrue($perm === (fileperms($tmpFile) & 0644), 'File permission was not correct: ' . decoct($perm));
 
         /* @noinspection PhpIncludeInspection */
         require $tmpFile;
