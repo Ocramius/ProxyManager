a0276470a9db1ef8f680099d789a2f6afa93d36d
Marco Pivetta <ocramius@gmail.com>
2018-08-25 22:30:59 +0200

    Documented `SignatureGeneratorInterface parameters`

--- src/ProxyManager/Signature/SignatureGeneratorInterface.php
+++ src/ProxyManager/Signature/SignatureGeneratorInterface.php
@@ -12,11 +12,15 @@ interface SignatureGeneratorInterface
 {
     /**
      * Generates a signature to be used to verify generated code validity
+     *
+     * @param mixed[] $parameters
      */
     public function generateSignature(array $parameters) : string;
 
     /**
      * Generates a signature key to be looked up when verifying generated code validity
+     *
+     * @param mixed[] $parameters
      */
     public function generateSignatureKey(array $parameters) : string;
 }
