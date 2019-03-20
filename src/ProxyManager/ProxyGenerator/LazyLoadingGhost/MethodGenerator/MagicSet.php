cd6f1ee4c9b7f4a29c949e4da45a0b451b039fe6
Marco Pivetta <ocramius@gmail.com>
2019-03-20 15:19:36 +0100

    Making sure that `__set` with `: void` does not return an expression

--- src/ProxyManager/ProxyGenerator/LazyLoadingGhost/MethodGenerator/MagicSet.php
+++ src/ProxyManager/ProxyGenerator/LazyLoadingGhost/MethodGenerator/MagicSet.php
@@ -21,31 +21,33 @@ use function sprintf;
  */
 class MagicSet extends MagicMethodGenerator
 {
-    /** @var string */
-    private $callParentTemplate = <<<'PHP'
-%s
+    private const TEMPLATE = <<<'PHP'
+$this->%initializerProperty% && $this->%callInitializer%('__set', ['name' => $name, 'value' => $value]);
 
-if (isset(self::$%s[$name])) {
-    return ($this->$name = $value);
+if (isset(self::$%publicProperties%[$name])) {
+    %returnNonVoidExpression%($this->$name = $value);
+    %returnVoidExpression%
 }
 
-if (isset(self::$%s[$name])) {
+if (isset(self::$%protectedProperties%[$name])) {
     // check protected property access via compatible class
     $callers      = debug_backtrace(\DEBUG_BACKTRACE_PROVIDE_OBJECT, 2);
     $caller       = isset($callers[1]) ? $callers[1] : [];
     $object       = isset($caller['object']) ? $caller['object'] : '';
-    $expectedType = self::$%s[$name];
+    $expectedType = self::$%protectedProperties%[$name];
 
     if ($object instanceof $expectedType) {
-        return ($this->$name = $value);
+        %returnNonVoidExpression%($this->$name = $value);
+        %returnVoidExpression%
     }
 
     $class = isset($caller['class']) ? $caller['class'] : '';
 
     if ($class === $expectedType || is_subclass_of($class, $expectedType) || $class === 'ReflectionProperty') {
-        return ($this->$name = $value);
+        %returnNonVoidExpression%($this->$name = $value);
+        %returnVoidExpression%
     }
-} elseif (isset(self::$%s[$name])) {
+} elseif (isset(self::$%privateProperties%[$name])) {
     // check private property access via same class
     $callers = debug_backtrace(\DEBUG_BACKTRACE_PROVIDE_OBJECT, 2);
     $caller  = isset($callers[1]) ? $callers[1] : [];
@@ -53,7 +55,7 @@ if (isset(self::$%s[$name])) {
 
     static $accessorCache = [];
 
-    if (isset(self::$%s[$name][$class])) {
+    if (isset(self::$%privateProperties%[$name][$class])) {
         $cacheKey = $class . '#' . $name;
         $accessor = isset($accessorCache[$cacheKey])
             ? $accessorCache[$cacheKey]
@@ -61,11 +63,12 @@ if (isset(self::$%s[$name])) {
                 return ($instance->$name = $value);
             }, null, $class);
 
-        return $accessor($this, $value);
+        %returnNonVoidExpression%$accessor($this, $value);
+        %returnVoidExpression%
     }
 
     if ('ReflectionProperty' === $class) {
-        $tmpClass = key(self::$%s[$name]);
+        $tmpClass = key(self::$%privateProperties%[$name]);
         $cacheKey = $tmpClass . '#' . $name;
         $accessor = isset($accessorCache[$cacheKey])
             ? $accessorCache[$cacheKey]
@@ -73,11 +76,12 @@ if (isset(self::$%s[$name])) {
                 return ($instance->$name = $value);
             }, null, $tmpClass);
 
-        return $accessor($this, $value);
+        %returnNonVoidExpression%$accessor($this, $value);
+        %returnVoidExpression%
     }
 }
 
-%s
+%parentAccess%
 PHP;
 
     /**
@@ -97,11 +101,11 @@ PHP;
             [new ParameterGenerator('name'), new ParameterGenerator('value')]
         );
 
-        $override = $originalClass->hasMethod('__set');
+        $returnType   = $this->getReturnType();
+        $isVoid       = $returnType && 'void' === strtolower($returnType->generate());
+        $parentAccess = ($isVoid ? '' : 'return ') . 'parent::__set($name, $value);';
 
-        $parentAccess = 'return parent::__set($name, $value);';
-
-        if (! $override) {
+        if (! $originalClass->hasMethod('__set')) {
             $parentAccess = PublicScopeSimulator::getPublicAccessSimulationCode(
                 PublicScopeSimulator::OPERATION_SET,
                 'name',
@@ -109,17 +113,21 @@ PHP;
             );
         }
 
-        $this->setBody(sprintf(
-            $this->callParentTemplate,
-            '$this->' . $initializerProperty->getName() . ' && $this->' . $callInitializer->getName()
-            . '(\'__set\', array(\'name\' => $name, \'value\' => $value));',
-            $publicProperties->getName(),
-            $protectedProperties->getName(),
-            $protectedProperties->getName(),
-            $privateProperties->getName(),
-            $privateProperties->getName(),
-            $privateProperties->getName(),
-            $parentAccess
+        $replacements = [
+            '%initializerProperty%'     => $initializerProperty->getName(),
+            '%callInitializer%'         => $callInitializer->getName(),
+            '%publicProperties%'        => $publicProperties->getName(),
+            '%protectedProperties%'     => $protectedProperties->getName(),
+            '%privateProperties%'       => $privateProperties->getName(),
+            '%parentAccess%'            => $parentAccess,
+            '%returnVoidExpression%'    => $isVoid ? 'return;' : '',
+            '%returnNonVoidExpression%' => $isVoid ? '' : 'return ',
+        ];
+
+        $this->setBody(str_replace(
+            array_keys($replacements),
+            $replacements,
+            self::TEMPLATE
         ));
     }
 }
