b1afa6775e7557ef08aa061e28321ec07c791d39
Marco Pivetta <ocramius@gmail.com>
2019-05-25 02:23:05 +0200

    Deprecated `SmartReferenceInterface` (unused)

--- src/ProxyManager/Proxy/SmartReferenceInterface.php
+++ src/ProxyManager/Proxy/SmartReferenceInterface.php
@@ -6,6 +6,8 @@ namespace ProxyManager\Proxy;
 
 /**
  * Smart reference object marker
+ *
+ * @deprecated this interface is not in use anymore, and should not be relied upon
  */
 interface SmartReferenceInterface extends ProxyInterface
 {
