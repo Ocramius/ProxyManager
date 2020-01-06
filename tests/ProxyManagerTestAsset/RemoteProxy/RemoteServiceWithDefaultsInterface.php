52b2eee96c811766fe4184ebc11de59fb0dc9d30
Pedro Tanaka <pedro.stanaka@gmail.com>
2020-01-06 10:23:42 +0100

    Fixing PHPCS offences

--- tests/ProxyManagerTestAsset/RemoteProxy/RemoteServiceWithDefaultsInterface.php
+++ tests/ProxyManagerTestAsset/RemoteProxy/RemoteServiceWithDefaultsInterface.php
@@ -6,13 +6,10 @@ namespace ProxyManagerTestAsset\RemoteProxy;
 
 /**
  * Simple interface for a remote API that methods with default parameters
- *
- * @author Pedro Tanaka <pedro.stanaka@gmail.com>
- * @license MIT
  */
 interface RemoteServiceWithDefaultsInterface
 {
-    public function optionalNonNullable(string $foo, string $optional = ''): int;
+    public function optionalNonNullable(string $foo, string $optional = '') : int;
 
-    public function optionalNullable(string $foo, ?float $nullable = null): int;
+    public function optionalNullable(string $foo, ?float $nullable = null) : int;
 }
